70cfc978a348632e0307e18a69d55107b059b251
liuliangdong <129725229+liuliangdong@users.noreply.github.com>
2023-05-04 12:58:22 +0800

    Add files via upload

---
+++ study20230504/study20230504/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Book
+{
+	char name[20];
+	short price;
+};
+int main()
+{
+	struct Book b1 = {"C语言程序设计", 55};
+	printf("书名:%s\n", b1.name);
+	printf("价格:%d\n", b1.price);
+	return 0;
+
+}
